474d66adb08458f61b3a4182960bf4ea8096d26c
Ken669 <hemetuzo@honeys.be>
2020-07-26 21:04:01 +0900

    describe EndPoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, only: :create
+
+  get 'posts/:id', to: 'posts#checked'
+  # use Path Parameter as End Point
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
