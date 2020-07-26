c11d6a68fcb1a26c8a8fdc2b2611e81ed78ee648
Ken669 <hemetuzo@honeys.be>
2020-07-26 19:39:05 +0900

    order:DESC + change index.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
+    @posts = Post.all.order(id: 'DESC')
     @post = Post.new
   end
 
   def create
     Post.create(post_params)
+    redirect_to '/'
   end
 
   private
