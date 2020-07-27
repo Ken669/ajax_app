e3efa56bda8478e7be5e76994025347aa43dba27
Ken669 <hemetuzo@honeys.be>
2020-07-27 11:54:48 +0900

    front side coding(4-03)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,14 +11,17 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
+
     if post.checked then
       post.update(checked: false)
     else
       post.update(checked: true)
     end
+    # checkedカラムの値を反転
 
     item = Post.find(params[:id])
     render json: {post: item}
+    # クリックした投稿のレコード情報をJson形式でレスポンス
   end
 
   private
